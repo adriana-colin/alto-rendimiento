3a5e54e73e9efff379376104946cba6b5153b7af
Adriana Colin <adriana_3127230713@utd.edu.mx>
2024-05-27 14:16:56 -0600

    Tablero y figuras funcionando

--- JuegoGato.py
+++ JuegoGato.py
@@ -1,10 +1,11 @@
 import turtle 
 
 class TicTacToeTablero():
-
+    
     def __init__(self, tamaño = 200):
         self.tamaño = tamaño
         self.t = turtle.Turtle()
+        self.centros = {}
 
     def dibuja_tablero(self):
         #Líneas verticales
@@ -22,15 +23,14 @@ class TicTacToeTablero():
             self.t.goto(self.tamaño // 2, self.tamaño // 6 - i * self.tamaño //3)
 
     def dibuja_centros(self):
-        centros = {}
-        
+
         #Puntos de los centros 1,4,7
         for i in range(3):
             self.t.penup()
             self.t.goto(-self.tamaño // 3, self.tamaño // 3 - i * self.tamaño // 3)
             self.t.pendown()
             self.t.dot(10)
-            centros[3 * i + 1] = self.t.position()
+            self.centros[3 * i + 1] = self.t.position()
 
         #Puntos de los centros 2,5,8
         for i in range(3):
@@ -38,7 +38,7 @@ class TicTacToeTablero():
             self.t.goto(0, self.tamaño // 3 - i * self.tamaño // 3)
             self.t.pendown()
             self.t.dot(10)
-            centros[3 * i + 2] = self.t.position()
+            self.centros[3 * i + 2] = self.t.position()
 
         #Puntos de los centros 3,6,9
         for i in range(3):
@@ -46,19 +46,63 @@ class TicTacToeTablero():
             self.t.goto(self.tamaño // 3 , self.tamaño // 3 - i * self.tamaño // 3)
             self.t.pendown()
             self.t.dot(10)
-            centros[3 * i + 3] = self.t.position()
+            self.centros[3 * i + 3] = self.t.position()
 
-            
-        return centros
-
-
+        return self.centros
+    
+    
     def display(self):
         screen = turtle.Screen()
         self.dibuja_tablero()
         centros = self.dibuja_centros()
         print(centros)
-        screen.mainloop()
+        #screen.mainloop()
+
+class circulo():
+    
+    t = turtle.Turtle()
+
+    def __init__(self, radio = 20):
+        self.radio = radio
+        self.t = turtle.Turtle()
+
+    def dibujar():
+        t = turtle.Turtle()
+        t.penup()
+        t.goto(0,100)
+        t.left(270)
+        t.forward(40)
+        t.left(90)
+        t.pendown()
+        t.circle(40)
+
+class tachita():
+    
+    t = turtle.Turtle()
+
+    def __init__(self, tamaño = 28):
+        #self.t = turtle.Turtle()
+        self.tamaño = float(tamaño)
+
+    def dibujar():
+        t = turtle.Turtle()
+        t.penup()
+        t.goto(-100,100)
+        t.left(45)
+        t.forward(40)
+        t.left(180)
+        t.pendown()
+        t.forward(80)
+        t.penup()
+        t.goto(-71.72,71.72)
+        t.right(90)
+        t.pendown()
+        t.forward(80)
 
 if __name__ == "__main__":
     tablero = TicTacToeTablero(tamaño=300)
     tablero.display()
+    circulo.dibujar()
+    tachita.dibujar()
+    screen = turtle.Screen()
+    screen.mainloop()
